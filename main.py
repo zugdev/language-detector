7fa543e5c6002a1c3c4be08f9e33df5c6476f635
zugdev <zug.dev@proton.me>
2024-12-06 16:33:50 -0300

    feat: fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import joblib
-import streamlit
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -34,3 +33,6 @@ async def predict_language(request: TextRequest):
     top_5_langs = sorted(lang_probs, key=lambda x: x[1], reverse=True)[:5]
 
     return {"top_5_languages": [{"language": lang, "probability": prob} for lang, prob in top_5_langs]}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
